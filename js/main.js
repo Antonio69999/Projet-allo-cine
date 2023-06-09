484761935822dbca7b943c9c0073d5c498a3f12a
VinceS42 <v.fayon.pro@gmail.com>
2023-06-09 14:37:17 +0200

    modif javaVF

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
-/** Test Récupération **/
+let movies;
 
-const imageElement = document.createElement("img");
-imageElement.src = https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg;
+// Récupération des pièces depuis le fichier JSON
 
-/** Rattachement balise DOM **/
+fetch("data/moovies.json")
+    .then((response) => response.json)
+    .then((data) => {
+
+    movies = data;
+    console.log(movies);
+    })
+
+
+// for (var i = 0; i < heroes.length; i++) {    
+//      myH2 = document.createElement('h2');
+//      myArticle = document.createElement('article');
+//      myPara1 = document.createElement('img');
+//      myPara2 = document.createElement('p');
+//    }
+ 
+
+// const imageElement = document.createElement("img");
+// imageElement.src = moovies.image;
+// const nomElement = document.createElement("h2");
+// nomElement.innerText = moovies.nom;
+// const prixElement = document.createElement("p");
+
+// const categorieElement = document.createElement("p");
+// categorieElement.innerText = moovies.categorie;
+
+// const sectionFiches = document.querySelector(".Movies");
+// Movies.appendChild(imageElement);
+// Movies.appendChild(nomElement);
+// Movies.appendChild(prixElement);
+// Movies.appendChild(categorieElement);
+
