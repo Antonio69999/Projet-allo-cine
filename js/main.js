6bc4d0cd32adcf08705e7380f059c5133c1d54eb
VinceS42 <v.fayon.pro@gmail.com>
2023-06-09 10:31:27 +0200

    modif javaVF

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/** Test Récupération **/
+
+const imageElement = document.createElement("img");
+imageElement.src = https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg;
+
+/** Rattachement balise DOM **/
